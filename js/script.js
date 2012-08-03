d9a12fd3e529e47b0cb017f32dbaa44e3dcfb035
francisbyrne <byrnef87@gmail.com>
2012-08-03 20:50:31 +1000

    new design - scrolly
    added new funky scroll functionality for landing page.

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/* Author:
+
+*/
+
+
+$(document).ready(function() {
+//$('#nav').localScroll(800);
+	
+	RepositionNav();
+	
+	$(window).resize(function(){
+		RepositionNav();
+	});	
+	
+	//.parallax(xPosition, adjuster, inertia, outerHeight) options:
+	//xPosition - Horizontal position of the element
+	//adjuster - y position to start from
+	//inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
+	//outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
+	$('#intro').parallax("50%", 0, 0.1, true);
+	$('#second').parallax("25%", 2800, 0.1, true);
+	$('#fourth').parallax("5%", 4050, 0.6, true);
+	
+
+  var deck = new $.scrolldeck({
+  	slides: '.slide',
+		buttons: '#nav li a',
+		easing: 'easeInOutExpo'
+  });
+});
+
+
+
+
