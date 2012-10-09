e61ac889c294cec0ce9e9b2fd7449596ecf7103a
francisbyrne <byrnef87@gmail.com>
2012-10-09 23:41:31 +1100

    Made nav highlighting work

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -20,5 +20,14 @@ function GoalsCtrl($scope, $filter) {
 	};
 
 }
-
 GoalsCtrl.$inject = ['$scope', '$filter'];
+
+function NavCtrl($scope, $location) { 
+
+        $scope.navClass = function (page) {
+        var currentRoute = $location.path().substring(1) || 'home';
+        return page === currentRoute ? 'active' : '';
+    };   
+
+}
+NavCtrl.$inject = ['$scope', '$location'];
